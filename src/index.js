251ea416bf4b964c50280d81d22628245510cf73
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-03-10 22:07:38 +0100

    last

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from "react-redux";
+import { BrowserRouter } from 'react-router-dom'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider>
-        <App />
-      </Provider>
+    <BrowserRouter> 
+        <App /> 
     </BrowserRouter>
   </React.StrictMode>
 );
