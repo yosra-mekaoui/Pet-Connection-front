b68f7f666111c30789ec7c5dc20a55bcc8bbdda9
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-03-25 10:14:33 +0100

    second

--- src/Components/Pages/AssociationList.js
+++ src/Components/Pages/AssociationList.js
@@ -71,45 +71,72 @@ function AssociationList() {
             </div>
           </div>
         </div>
-        
-        <h2 style={{marginTop : "40px", marginLeft : "8%", color:"black", marginBottom : "-20px"  }}>
-          You have an association? Become a <NavLink to="/upgrade">Partner.</NavLink>
+
+        <h2
+          style={{
+            marginTop: "40px",
+            marginLeft: "8%",
+            color: "#353535",
+            marginBottom: "-20px",
+          }}
+        >
+          You have an association? Become a{" "}
+          <NavLink to="/upgrade">Partner.</NavLink>
         </h2>
 
-        <div className="blog-grid-pages pt-120 mb-120">
-          <div className="container">
-            <div className="row g-lg-4 gy-5 justify-content-center mb-70">
-              {data.map((item) => (
-                <div className="col-lg-6 col-md-6 col-sm-10">
-                  <div className="h1-blog-card">
-                    <div className="blog-img">
-                      <img
-                        className="img-fluid"
-                        src={baseUrl + item.image}
-                        alt=""
-                      />
-                      <div className="category">
-                        <a href="blog-grid.html">{item.name}</a>
+        <div className="container">
+          <div className="row">
+            <div className="col-6" style={{ marginTop: "120px" }}>
+              <div
+                style={{
+                  boxShadow:
+                    "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+                  padding: "20px",
+                  borderRadius: "10px",
+                  background:
+                    "linear-gradient(217deg, rgba(255,223,195,.8), rgba(255,223,195,0) 70.71%),            linear-gradient(127deg, rgba(230,207,207,.8), rgba(230,207,207,0) 70.71%),            linear-gradient(336deg, rgba(255,225,225,.8), rgba(255,225,225,0) 70.71%)",
+                }}
+              >
+                <h3 style={{ paddingLeft: "10px", color: "#353535" }}>
+                  What's New ?{" "}
+                </h3>
+              </div>
+            </div>
+
+            <div className="col-6">
+              <div className="blog-grid-pages pt-120 mb-120">
+                <div className="container">
+                  <div className="row g-lg-4 gy-5 justify-content-center mb-70">
+                    {data.map((item) => (
+                      <div className="col-lg-12 col-md-6 col-sm-10">
+                        <div className="h1-blog-card">
+                          <div className="blog-img">
+                            <img
+                              className="img-fluid"
+                              src={baseUrl + item.image}
+                              alt=""
+                            />
+                            <div className="category">
+                              <a href="blog-grid.html">{item.name}</a>
+                            </div>
+                          </div>
+                          <div className="blog-content">
+                            <div className="blog-meta">
+                              <a href="blog-grid.html">{item.role}</a>
+                            </div>
+                            <h4>
+                              <a href="blog-details.html">{item.bio}</a>
+                            </h4>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                    <div className="blog-content">
-                      <div className="blog-meta">
-                        <a href="blog-grid.html">{item.role}</a>
-                      </div>
-                      <h4>
-                        <a href="blog-details.html">{item.bio}</a>
-                      </h4>
-                    </div>
+                    ))}
                   </div>
                 </div>
-              ))}
+              </div>
             </div>
           </div>
         </div>
-
-        
-            
-
       </>
     );
 }
