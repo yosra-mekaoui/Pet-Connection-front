b98aea2822e9893721421b13e3758faa512961a4
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-03-26 16:41:40 +0200

    one association 4

--- src/Components/Pages/Association.js
+++ src/Components/Pages/Association.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { NavLink, useParams } from "react-router-dom";
 import axios from "axios";
 import { getOneAssociation } from "./api";
+import { format } from "date-fns";
 function AssociationList() {
     const { id } = useParams(); 
     const [data, setData] = useState({});
@@ -19,27 +20,15 @@ function AssociationList() {
         `http://localhost:3000/association/getOneAssociation/${id}`
       )
       .then((response) => {
-          setData(response.data);
-          const d = Date(response.data.date); 
-          console.log("date : " + d); 
-          const formattedDate = format(d, "dd-MM-yyyy");
-        //   formattedDate = d.toLocaleDateString("en-US", {
-        //     month: "numeric",
-        //     day: "numeric",
-        //     year: "numeric",
-        //   });
-
-          console.log("Formattttted : " + response.data.date);
+          setData(response.data); 
 
       })
       .catch((error) => {
         console.log(error);
       });
-
-    
-      //console.log("Formattttted : " + formattedDate);
+ 
     axios
-      .get("http://localhost:3000/funding/allFunding")
+      .get(`http://localhost:3000/funding/getFundingByAssociation/${id}`)
       .then((response) => {
         setFundings(response.data);
         console.log(fundings);
@@ -110,12 +99,14 @@ function AssociationList() {
               }}
             >
               <h3 style={{ color: "black", fontWeight: "700" }}>{data.name}</h3>
-                <p style={{
-                        fontStyle: "italic",
-                        fontSize : "19px"
-                    }}> 
-                      Released on {formattedDate}
-                </p>
+              <p
+                style={{
+                  fontStyle: "italic",
+                  fontSize: "19px",
+                }}
+              >
+                Released on {data.date}
+              </p>
               <p style={{ color: "#494949" }}>{data.bio}</p>
 
               <center>
@@ -123,20 +114,58 @@ function AssociationList() {
                   src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d82077.28159616987!2d${data.longitude}!3d${data.latitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2stn!4v1679836721177!5m2!1sfr!2stn`}
                   width={"90%"}
                   height={450}
-                  style={{ border: "2px solid gray" }}
+                  style={{ border: "2px solid gray", marginTop: "30px" }}
                   allowFullScreen
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
                 />
               </center>
-              {/* {fundings.map((item) => (
+            </div>
+          </div>
+
+          <div className="col-6">
+            {/* =========== IMAGE ================= */}
+            <img
+              style={{
+                marginTop: "50px",
+                boxShadow:
+                  "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+                borderRadius: "10px",
+                display: "block",
+                maxWidth: "100%",
+                height: "auto",
+              }}
+              src={baseUrl + data.image}
+            />
+
+            {/* ============== CROWDFUNDINGS PER ASSOCIATION ============== */}
+            <div
+              style={{
+                marginTop: "25px",
+                paddingLeft: "30px",
+                boxShadow:
+                  "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+                padding: "20px",
+                borderRadius: "10px",
+                background:
+                  "linear-gradient(217deg, rgba(255,223,195,.8), rgba(255,223,195,0) 70.71%), linear-gradient(127deg, rgba(230,207,207,.8), rgba(230,207,207,0) 70.71%),            linear-gradient(336deg, rgba(255,225,225,.8), rgba(255,225,225,0) 70.71%)",
+              }}
+            >
+              <h3 style={{ color: "black " }}>Latest Actions</h3>
+
+              {fundings.length == 0 && (
+                <h5 style={{ color: "#494949", marginTop : "20px" }}>
+                  {data.name} hasn't done any actions yet.
+                </h5>
+              )}
+              {fundings.map((item) => (
                 <>
                   <div
                     style={{
                       marginLeft: "10px",
                       color: "black",
                       fontSize: "18px",
-                      marginTop: "40px",
+                      marginTop: "30px",
                       fontWeight: "700px !important",
                     }}
                   >
@@ -184,23 +213,8 @@ function AssociationList() {
                     </NavLink>
                   </div>
                 </>
-              ))} */}
+              ))}
             </div>
-          </div>
-
-          <div className="col-6">
-            <img
-              style={{
-                marginTop: "50px",
-                boxShadow:
-                  "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
-                borderRadius: "10px",
-                display: "block",
-                maxWidth: "100%",
-                height: "auto",
-              }}
-              src={baseUrl + data.image}
-            />
           </div>
         </div>
       </div>
