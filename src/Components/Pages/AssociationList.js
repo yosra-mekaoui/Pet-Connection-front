1af638df754d8b79d67ff2b88461200027f5d5a0
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-03-28 00:59:56 +0200

    done payment second

--- src/Components/Pages/AssociationList.js
+++ src/Components/Pages/AssociationList.js
@@ -6,7 +6,8 @@ function AssociationList() {
     const [fundings, setFundings] = useState([]);
     const [color, setColor] = useState("");
     const [img, setImg] = useState("http://localhost:3000/uploads/");
-
+    const [total, setTotal] = useState(0) ;
+    const [funding, setFunding] = useState(""); 
 
     const baseUrl = "http://localhost:3000/associations/"; 
 
@@ -51,13 +52,17 @@ function AssociationList() {
     const query = new URLSearchParams(window.location.search);
 
     if (query.get("success")) {
-      setMessage("Order placed! You will receive an email confirmation.");
+      setTotal(query.get("total")); 
+      setFunding(query.get("funding")); 
+      setMessage(
+        "Every contribution is highly valued, and we are committed to ensuring that your donation is utilized in the most effective manner. We appreciate your trust in us and thank you once again for your generous support. "
+      );
       setColor("green"); 
     }
 
     if (query.get("canceled")) {
       setMessage(
-        "Order canceled -- continue to shop around and checkout when you're ready."
+        "Please check the payment information you entered as an error has occurred with your payment method. Make sure that all the details you have provided are correct."
       );
       setColor("red"); 
     }
@@ -115,17 +120,59 @@ function AssociationList() {
         </div>
         {message && (
           <center>
-            <h1 style={{
-              marginTop: "70px",
-              marginBottom: "100px",
-              width: "70%",
-              color : color
-            }}>
-              {message}
-            </h1> 
+            <div
+              style={{
+                boxShadow:
+                  "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+                padding: "20px",
+                borderRadius: "10px",
+                width: "80%",
+                marginTop: "50px",
+                marginBottom: "50px",
+                background:
+                  "linear-gradient(217deg, rgba(255,223,195,.8), rgba(255,223,195,0) 70.71%), linear-gradient(127deg, rgba(230,207,207,.8), rgba(230,207,207,0) 70.71%),            linear-gradient(336deg, rgba(255,225,225,.8), rgba(255,225,225,0) 70.71%)",
+              }}
+            >
+              <h5
+                style={{
+                  marginTop: "40px",
+                  marginBottom: "50px",
+                  width: "90%",
+                  color: color,
+                }}
+              >
+                {message}
+              </h5>
+
+              <div
+                style={{
+                  marginBottom: "60px",
+                  color: "black",
+                  fontWeight: "700",
+                  fontSize: "24px",
+                }}
+              >
+                {funding && (
+                  <>
+                    <i className="fa fa-check" style={{ fontSize: 24 }} />
+                    &nbsp;{total}$ successfully sent to {funding}.
+                    <h5
+                      style={{
+                        marginTop: "40px",
+                        marginBottom: "50px",
+                        width: "90%",
+                        color: color,
+                      }}
+                    >
+                      Thank you for your donation.
+                    </h5>
+                  </>
+                )}
+              </div>
+            </div>
           </center>
         )}
-        
+
         {!message && (
           <>
             <h2
